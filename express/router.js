b775f70a0d9a7abaf73c6e1becaae392632cfd96
blessykbabu <blessykbabu5@gmail.com>
2023-11-13 13:16:00 +0530

    image folder

--- express/router.js
+++ express/router.js
@@ -1,7 +1,15 @@
 import { Router } from "express";
 import auth from "./middlewares/auth.js";
+import multer from 'multer'
+import path from 'path'
 
 import * as userHandlers from "./request-handler.js";
+const storage =multer.diskStorage({
+    destination:"./files",
+    filename:(req,file,cb)=>{
+    cb(null,file.originalname);
+}})
+const upload=multer({storage:storage});//storage objct
 
 const router = Router()
 
@@ -11,5 +19,12 @@ router.route("/profile").get(auth, userHandlers.profile);
 
 router.route("/add-note").post(auth, userHandlers.addNote);
 router.route("/get-note").get(auth,userHandlers.getNote)
-
+router.route("/file").post(upload.single("myfile"),(req,res)=>{
+    console.log(req.file);
+    res.json("file stored")
+})//myfile is a input filed name
+router.route("/get-file/:file").get((req,res)=>{
+    let fileName=req.params;
+    res.sendFile(path.resolve(`./files/${fileName.file}`));
+})
 export default router;
